27fbe0f7ef1db2c71dc91937e6f4817cfc662b4d
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-05-08 13:12:00 -0400

    refactored the solution to be a bit cleaner

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // DAY 1: Write a function createHelloWorld. It should return a new function that always returns the string 'Hello World'.
-const createHelloWorld = () => { return (...args) => { return 'Hello World' } };
-console.log(createHelloWorld()()); // 'Hello World'
+const createHelloWorld = () => (...args) => "Hello World";
+console.log(createHelloWorld()());
