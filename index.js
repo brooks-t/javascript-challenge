27f0efaa924280adb40f08bf3b1b0eeccfb56987
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-05-10 11:43:59 -0400

    completed days 4 and 5

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ console.log(createHelloWorld()());
 =================================================================*/
 
 
-// DAY 2: (2620) Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 /*=================================================================
+DAY 2: (2620) Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 console.log('=== DAY 2 ===');
 
 let n = 0;
@@ -20,11 +20,12 @@ console.log(counter());
 console.log(counter());
 =================================================================*/
 
-
-/* DAY 3: (2665) Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+/*=================================================================
+DAY 3: (2665) Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
 The three functions are: 1) increment() increases the current value by 1 and then returns it.
 2) decrement() decreases the current value by 1 and then returns it. 3) reset() sets the current value to init and then returns it. */
-console.log('=== DAY 3 ===');
+
+/* console.log('=== DAY 3 ===');
 
 const createCounter2 = (init) => {
     let current = init;
@@ -37,7 +38,41 @@ const createCounter2 = (init) => {
 }
 
 const counter2 = createCounter2(0);
-// note that when you're calling the function, you're not passing in any arguments, because the argument is already set to 0 in the createCounter2 function
+
 console.log(counter2.increment()); // note that this skips 'let current = init' and the console.log; and goes straight to ++current
 console.log(counter2.reset());
 console.log(counter2.decrement());
+=========================================================================================*/
+
+/*===============================================================
+DAY 4: (2635) Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element. The returned array should be created such that returnedArray[i] = fn(arr[i], i). Please solve it without the built-in Array.map method.
+console.log('=== DAY 4 ===');
+var map = function (arr, fn) {
+    let returnedArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        returnedArr.push(fn(arr[i], i)); // fn is multiplying each element by its index and pushing it to the returnedArr
+    }
+    return returnedArr;
+}
+
+const arr = [1, 2, 3, 4, 5];
+const fn = (x, i) => x * i;
+console.log(map(arr, fn));
+=================================================================*/
+
+/*===============================================================
+DAY 5: (2634) Given an integer array arr and a filtering function fn, return a new array with a fewer or equal number of elements. The returned array should only contain elements where fn(arr[i], i) evaluated to a truthy value. Please solve it without the built-in Array.filter method. */
+console.log('=== DAY 5 ===');
+const filter = (arr, fn) => {
+    let returnedArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            returnedArr.push(arr[i]);
+        }
+    }
+    return returnedArr;
+}
+
+const arr = [1, 2, 3, 4, 5];
+const fn = (x, i) => x % 2 === 0;
+console.log(filter(arr, fn));
